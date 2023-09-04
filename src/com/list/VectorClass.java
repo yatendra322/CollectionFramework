d344c86899a508649439324929699e8cf21f335c
Praveen <praveenthakur0962@gmail.com>
2023-09-04 17:39:37 +0530

    VectorClass

--- src/com/list/VectorClass.java
+++ src/com/list/VectorClass.java
@@ -1,80 +1,120 @@
 package com.list;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import com.input.GetInput;
 import com.interface1.ListInterfaces;
 
-public class VectorClass implements ListInterfaces{
-	
+public class VectorClass implements ListInterfaces {
+
 	static VectorClass vc;
-	
+
 	private List<String> vectorClass;
-	
+
 	private VectorClass() {
-		this.vectorClass=new ArrayList<String>();
+		this.vectorClass = new ArrayList<String>();
 	}
-public static VectorClass getInstance() {
-		if(vc!=null) {
+
+	public static VectorClass getInstance() {
+		if (vc != null) {
 			return vc;
 		}
-		vc=new VectorClass();
+		vc = new VectorClass();
 		return null;
-		
 	}
 
 	@Override
 	public void insertElement() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Enter the new record :-");
+		this.vectorClass.add(GetInput.getInstance().getNewRecord());
+		System.out.println("Record added successfully !!!");
 	}
 
 	@Override
 	public void insertMutipleElement() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Enter the limit of entired records ");
+		int limit = GetInput.getInstance().getChoiceNo();
+		System.out.println("Enter the " + limit + "records ");
+		for (int i = 0; i < limit; i++) {
+			this.vectorClass.add(GetInput.getInstance().getNewRecord());
+		}
+		System.out.println("Record added successfully !!!");
 	}
 
 	@Override
 	public void updateElement() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Enter the update record ");
+		String olEl = GetInput.getInstance().getNewRecord();
+		if (this.vectorClass.contains(olEl)) {
+			System.out.println("Enter the new element for replace this :" + olEl);
+			String nwEl = GetInput.getInstance().getNewRecord();
+
+			for (int i = 0; i < this.vectorClass.size(); i++) {
+				if (this.vectorClass.get(i).toString().equals(olEl)) {
+
+					this.vectorClass.set(i, nwEl);
+					System.out.println("reocrd update successfully !!!");
+					break;
+				}
+
+			}
+		} else {
+			System.out.println(olEl + "this data is not available in records , So please try again");
+			updateElement();
+		}
 	}
 
 	@Override
 	public void retrieve() {
-		// TODO Auto-generated method stub
-		
+		if (this.vectorClass.isEmpty()) {
+			System.out.println("There is no records ");
+			return;
+		}
+		for (String x : this.vectorClass) {
+			System.out.println(x);
+		}
 	}
 
 	@Override
 	public void retrieveFirstAndLastElement() {
-		// TODO Auto-generated method stub
-		
+		if (this.vectorClass.size() > 0)
+
+			System.out.println(this.vectorClass.get(0) + "and" + this.vectorClass.get(this.vectorClass.size() - 1));
 	}
 
 	@Override
 	public void deleteElement() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Enter the record for delete ");
+		String el = GetInput.getInstance().getNewRecord();
+		if (vectorClass.contains(el)) {
+			vectorClass.remove(el);
+			System.out.println("Operation Successfully done !!!");
+
+		} else {
+			System.out.println("Record not found !!!");
+		}
 	}
 
 	@Override
 	public void deleteAllElement() {
-		// TODO Auto-generated method stub
-		
+		this.vectorClass.clear();
+		System.out.println("Operation successfully done !!!");
 	}
 
 	@Override
 	public void deleteSameElementButSaveOne() {
-		// TODO Auto-generated method stub
-		
+		Set<String> set = new HashSet<String>(this.vectorClass);
+		this.vectorClass.clear();
+		this.vectorClass.addAll(set);
 	}
 
 	@Override
 	public void shortElement() {
-		// TODO Auto-generated method stub
-		
+		this.vectorClass = this.vectorClass.stream().sorted().collect(Collectors.toList());
+		System.out.println(this.vectorClass);
 	}
-
 }
