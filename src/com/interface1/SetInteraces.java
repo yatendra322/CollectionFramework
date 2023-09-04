88d886b217d41b753f8075792b2a6890475b2657
Praveen <praveenthakur0962@gmail.com>
2023-09-04 18:22:50 +0530

    SetInterfaces

---
+++ src/com/interface1/SetInteraces.java
@@ -0,0 +1,21 @@
+package com.interface1;
+
+public interface SetInteraces {
+	
+	void insertElement();
+	
+	void insertMultipleElement();
+	
+	void updateElement();
+	
+	void retrieve();
+	
+	void retrieveFirstAndLastElement();
+	
+	void deleteElement();
+	
+	void deleteAllElement();
+	
+	void shortElement();
+
+}
